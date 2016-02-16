7cf0bc2c2b0dcbb3eb1421c63c27912b56cb43a5
Gema Gomez <gema@ggomez.me>
2016-02-16 14:34:54 +0000

    Changing default port.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,7 +4,7 @@
 from os import environ
 
 bind = "%s:%s" % (environ.get("IPADDR", '0.0.0.0'),
-                  environ.get("PORT", '11000'))
+                  environ.get("PORT", '5000'))
 debug = True
 workers = 1
 max_requests = 1
